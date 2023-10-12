d04c24d731888912fbf213410f288780dc884288
Pauline Pan <pan_pauline@yahoo.com>
2023-10-12 14:56:01 -0400

    Update and create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
-
+import ContactList from './ContactList'
 import './App.css'
+
+
 
 function App() {
 
-
+  //const [contacts, setContacts] = useState(dummyContacts)
+  //console.log("Contacts: " , contacts)
   return (
     <>
-
+      <ContactList />
     </>
   )
 }
